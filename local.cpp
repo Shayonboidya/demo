a8e96c038a706602001aa3ad4aa6aa17c35a62f3
Shayon Boidya <shayonboidya11@gmail.com>
2026-03-05 14:56:28 +0600

    add new file

---
+++ local.cpp
@@ -0,0 +1,15 @@
+/*Date: 05.03.2026*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout<<"Hello world"<<endl;
+
+
+    return 0;
+}
